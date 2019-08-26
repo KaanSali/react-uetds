d9c5896eaeb3b5586bd5eeb46221fab4e69ba35c
KaanSali <kaan_sali@hotmail.com>
2019-08-26 13:05:34 +0300

    Uygulama gite aktarıldı.

---
+++ components/Items.js
@@ -0,0 +1,76 @@
+import React from 'react';
+import { Text, Image, StyleSheet, View ,TouchableOpacity} from 'react-native';
+import { vw, vh, vmin, vmax } from 'react-native-expo-viewport-units';
+
+export class ListItem extends React.Component{
+  constructor(props){
+    super(props);
+  }
+  render(){
+    return (
+      <TouchableOpacity style={this.styles.shadow} onPress={() => this._onClickItem(this.props)} underlayColor="white">
+        <View style={[this.styles.itemContainer , this.styles.flex_row, this.styles.shadow]}>
+          <View style={this.styles.imageContainer}>
+            <Image source={this.props.image} style={this.styles.image}/>
+          </View>
+          <View style={this.styles.textContainer}>
+            <Text style={this.styles.usernameText}>{this.props.username}</Text>
+            <Text style={this.styles.userRoleText}>{this.props.userRole}</Text>
+          </View>
+        </View>
+      </TouchableOpacity>
+    );
+  }
+   _onClickItem(props){
+    alert(props.username + ' Kullanıcı profiline girildi');
+  }
+
+  styles = StyleSheet.create({
+    flex_row : {
+      flex:1,
+      flexDirection:'row'
+    },
+    shadow:{
+      backgroundColor: '#ffffff',
+/*       shadowColor: "#bfbfbf",
+      shadowOffset: {
+        width: 0,
+        height: 2,
+      },
+      shadowOpacity: 0.23,
+      shadowRadius: 2.62, */
+      borderColor:'#ffffff',
+      elevation: 3
+    },
+    itemContainer:{
+      width: vw(90),
+      height: vh(15),
+      alignSelf: 'center',
+      marginBottom: 5
+    },
+    textContainer:{
+      flex:1,
+      alignSelf:'center'
+    },
+    imageContainer:{
+    },
+    image:{ 
+      height: 80,
+      width: 80,
+      borderRadius: 80,
+      margin:20
+    },
+    usernameText: {
+      textAlign : 'center',
+      fontSize: 23,
+      fontFamily : 'Roboto'
+    },
+    userRoleText : {
+      marginBottom:0,
+      fontFamily : 'Roboto',
+      fontSize : 18,
+      textAlign : 'center'
+    }
+  })
+} 
+7 
